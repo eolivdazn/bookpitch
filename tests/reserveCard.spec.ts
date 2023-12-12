5f4f244d98111b912e60c687d8111a9be1d89b7d
eolivdazn <eterra1@gmail.com>
2023-12-12 18:41:37 +0100

    add visual test

---
+++ tests/reserveCard.spec.ts
@@ -0,0 +1,54 @@
+import {test, expect} from '@playwright/test';
+import * as process from "process";
+import {availability} from "./data/availability";
+
+test.describe('Reserve card', () => {
+    test.beforeEach(async ({page}) => {
+
+
+    });
+    const day = new Date().toISOString().split('T')[0];
+    test('Change reserveCard data', async ({page}) => {
+        await page.goto(process.env.NEXT_PUBLIC_URL as string);
+
+        await page.locator('id=select_time').selectOption('14:00:00.000Z');
+        await page.locator('id=btn_find_slots').click();
+        await page.waitForTimeout(1000);
+        expect(page.url()).toContain(`/search?sport=football&day=${day}&time=14:00:00.000Z&location=Guimaraes`);
+
+        await page.locator('id=select_sport').selectOption('volleyball');
+        await page.locator('id=btn_find_slots').click();
+        await page.waitForTimeout(1000);
+        expect(page.url()).toContain(`/search?sport=volleyball&day=${day}&time=14:00:00.000Z&location=Guimaraes`);
+
+        await page.locator('id=select_location').selectOption('Braga');
+        await page.locator('id=btn_find_slots').click();
+        await page.waitForTimeout(1000);
+        expect(page.url()).toContain(`/search?sport=volleyball&day=${day}&time=14:00:00.000Z&location=Braga`);
+
+    });
+
+    test.only('Change reserveCard set time', async ({page}) => {
+        let count = 0
+        await page.goto(process.env.NEXT_PUBLIC_URL + `?sport=football&day=${day}&time=20:00:00.000Z&location=Fafe`);
+        await page.locator('id=btn_find_slots').click();
+        await page.route('**/*', (route, request) => {
+
+
+            if (route.request().url().includes('availability?')) {
+                count = count + 1
+                route.fulfill({
+                    status: 200,
+                    contentType: 'application/json',
+                    body: JSON.stringify(availability),
+                });
+            }
+        });
+
+        await page.waitForTimeout(1000);
+        expect(count).toBe(2);
+        await page.screenshot({path: `tests/screenshots/${day}-reserveCard.png`});
+
+    });
+
+});
